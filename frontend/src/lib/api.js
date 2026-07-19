08c74223e4854f112d93d43f01f02f221d4b63dd
rakshit1305 <f20230213@dubai.bits-pilani.ac.in>
2026-07-19 20:50:48 +0530

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = "http://127.0.0.1:8000";
 export const API = `${BACKEND_URL}/api`;
 const client = axios.create({ baseURL: API, timeout: 120000 });
 export async function uploadDatasets(files) {
